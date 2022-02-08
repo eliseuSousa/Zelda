dff61c9afbf8ef88ab3e38a50c8a1455a6c87c04
eliseuSousa <eliseusousa759@gmail.com>
2022-02-08 16:26:18 -0300

    Create camera

--- code/level.py
+++ code/level.py
@@ -10,7 +10,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
                 
         # sprite group setup 
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         # sprite setup
@@ -25,10 +25,30 @@ class Level:
                 if col == 'x':
                     Tile((x, y),[self.visible_sprites, self.obstacle_sprites])
                 if col == 'p':
-                    Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        # General Setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+    
+    def custom_draw(self, player):
+
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
         
