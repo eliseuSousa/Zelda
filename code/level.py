6cc2df6f8b865276c926e54ac62f6889321f3233
eliseuSousa <eliseusousa759@gmail.com>
2022-02-13 20:29:23 -0300

    Limitando a area do jogador no mapa

--- code/level.py
+++ code/level.py
@@ -2,7 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
-from debug import debug
+from support import *
 class Level:
     def __init__(self):
 
@@ -17,15 +17,29 @@ class Level:
         self.create_map()   
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index*TILESIZE
-                y = row_index*TILESIZE
+
+        layouts = {
+            'boundary': import_csv_layout('../map/map_FloorBlocks.csv')
+        }
+
+        for style,layout in layouts.items():
+            for row_index,row in enumerate(layout):
+                for col_index,col in enumerate(row):
+                    if col != '-1':
+                        x = col_index*TILESIZE 
+                        y = row_index*TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'invisible')
+        # for row_index, row in enumerate(WORLD_MAP):
+        #     for col_index, col in enumerate(row):
+        #         x = col_index*TILESIZE
+        #         y = row_index*TILESIZE
                 
-                if col == 'x':
-                    Tile((x, y),[self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+        #         if col == 'x':
+        #             Tile((x, y),[self.visible_sprites, self.obstacle_sprites])
+        #         if col == 'p':
+        #             self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw game
